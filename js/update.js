3171d8f496a027d385a50bbd62736b7f95a8e95f
Simon Monecke <SimonMonecke@otto.de>
2017-02-02 08:54:58 +0100

    disable caching

--- js/update.js
+++ js/update.js
@@ -18,51 +18,51 @@ function transformState(activity, lastBuildStatus) {
 }
 
 function updateCache(pipelines, hiddenPrefix, defautHiddenSteps, cache, asynCall) {
-    $.each(pipelines, function(pipelineName, pipeline) {
+    $.each(pipelines, function (pipelineName, pipeline) {
         var hiddenSteps = pipeline['hiddenSteps'];
         var cctrayUrl = pipeline['cctrayUrl'];
         var guiURL = pipeline['guiUrl'];
         var guiURL = pipeline['visibleWaitingSteps'];
         $.ajax({
-                    type: 'GET',
-                    url: cctrayUrl,
-                    success: function (response) {
-                        var newProjectCache = new Array();
-                        $(response).find('Project').each(function (project) {
-                            var name = $(this).attr('name');
-                            var activity = $(this).attr('activity');
-                            var lastBuildStatus = $(this).attr('lastBuildStatus');
-                            var newState = transformState(activity, lastBuildStatus);
-                            var cacheEntry = [];
-                            //var keyName = pipeline['displayName'] + '-' + name;
-                            cacheEntry['stepName'] = name;
-                            cacheEntry['pipelineName'] = pipelineName;
-                            cacheEntry['state'] = newState;
-                            cacheEntry['visible'] = ($.inArray(name, hiddenSteps) == -1) &&
-                                                    ($.inArray(name, defautHiddenSteps) == -1) &&
-                                                    !(name.startsWith(hiddenPrefix)) &&
-                                                    ($.inArray(name, visibleWaitingSteps) != -1);
-                            cacheEntry['guiURL'] = guiURL;
-                            newProjectCache.push(cacheEntry);
-                        });
-                        cache[pipelineName] = newProjectCache;
-                    },
-                    error: function (response) {
-                        var newProjectCache = new Array();
+                type: 'GET',
+                url: cctrayUrl,
+                cache: false,
+                success: function (response) {
+                    var newProjectCache = new Array();
+                    $(response).find('Project').each(function (project) {
+                        var name = $(this).attr('name');
+                        var activity = $(this).attr('activity');
+                        var lastBuildStatus = $(this).attr('lastBuildStatus');
+                        var newState = transformState(activity, lastBuildStatus);
                         var cacheEntry = [];
-                        cacheEntry['stepName'] = '';
+                        //var keyName = pipeline['displayName'] + '-' + name;
+                        cacheEntry['stepName'] = name;
                         cacheEntry['pipelineName'] = pipelineName;
-                        cacheEntry['state'] = 'sick';
-                        cacheEntry['visible'] = true;
+                        cacheEntry['state'] = newState;
+                        cacheEntry['visible'] = ($.inArray(name, hiddenSteps) == -1) &&
+                            ($.inArray(name, defautHiddenSteps) == -1) && !(name.startsWith(hiddenPrefix)) &&
+                            ($.inArray(name, visibleWaitingSteps) != -1);
                         cacheEntry['guiURL'] = guiURL;
                         newProjectCache.push(cacheEntry);
-                        cache[pipelineName] = newProjectCache;
-                    }
-                    ,
-                    dataType: 'xml',
-                    crossDomain: true,
-                    async: asynCall
+                    });
+                    cache[pipelineName] = newProjectCache;
+                },
+                error: function (response) {
+                    var newProjectCache = new Array();
+                    var cacheEntry = [];
+                    cacheEntry['stepName'] = '';
+                    cacheEntry['pipelineName'] = pipelineName;
+                    cacheEntry['state'] = 'sick';
+                    cacheEntry['visible'] = true;
+                    cacheEntry['guiURL'] = guiURL;
+                    newProjectCache.push(cacheEntry);
+                    cache[pipelineName] = newProjectCache;
                 }
+                ,
+                dataType: 'xml',
+                crossDomain: true,
+                async: asynCall
+            }
         )
         ;
     });
