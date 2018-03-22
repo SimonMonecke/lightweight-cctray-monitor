0b5ac62c2df15443bf9fb1866657c2d901c77067
Simon Monecke <Simon.Monecke@otto.de>
2018-03-22 08:45:59 +0100

    remove deprecated regex compile function

--- js/core.js
+++ js/core.js
@@ -6,7 +6,7 @@ function lightweight_cctray_monitor(){
     updatePresentation(config.successText, cache, debug);
     setInterval(updatePresentation, 5000, config.successText, cache, debug);
 
-    var autoHiddenRegexCompiled = config.autoHiddenRegex !== undefined && config.autoHiddenRegex !== "" ? new RegExp(config.autoHiddenRegex).compile() : null;
+    var autoHiddenRegexCompiled = config.autoHiddenRegex !== undefined && config.autoHiddenRegex !== "" ? new RegExp(config.autoHiddenRegex) : null;
     updateCache(config.pipelines, autoHiddenRegexCompiled, config.defaultHiddenSteps, cache, true);
     setInterval(updateCache, 3000, config.pipelines, autoHiddenRegexCompiled, config.defaultHiddenSteps, cache, true);
 }
